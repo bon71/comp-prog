cedfd1130cf3bf17d83b29749cd9adc7288d3f2a
bon <badfatcat.git@gmail.com>
2024-08-23 21:10:17 +0900

    leet code mission of the day

--- 383.ransom-note.py
+++ 383.ransom-note.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
+        for letter in list(set(ransomNote)):
+            if ransomNote.count(letter) > magazine.count(letter): return False
+        return True
 # @lc code=end
-
--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -8,17 +8,10 @@
 class Solution:
     def reverse(self, x: int) -> int:
         pm = 1
-        if x == 0:
-            return x
         if x < 0:
             pm = -1
             x *= pm
-        list_x = list(str(x))
-        list_x.reverse()
-        str_result = ''
-        for str_x in list_x:
-            str_result = str_result + str_x
-        result = int(str_result) * pm
+        result = int(str(x)[::-1]) * pm
         if result > 2**31 or result < -2**31:
             return 0
         else:
