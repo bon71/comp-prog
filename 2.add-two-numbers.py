f792eda20f271f413e2316ebf6478c1245869c02
bon <badfatcat.git@gmail.com>
2024-09-13 23:01:51 +0900

    leet code mission of the day

---
+++ 2.add-two-numbers.py
@@ -0,0 +1,90 @@
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+# https://leetcode.com/problems/add-two-numbers/description/
+#
+# algorithms
+# Medium (43.96%)
+# Likes:    31620
+# Dislikes: 6340
+# Total Accepted:    4.9M
+# Total Submissions: 11.2M
+# Testcase Example:  '[2,4,3]\n[5,6,4]'
+#
+# You are given two non-empty linked lists representing two non-negative
+# integers. The digits are stored in reverse order, and each of their nodes
+# contains a single digit. Add the two numbers and return the sum as a linked
+# list.
+# 
+# You may assume the two numbers do not contain any leading zero, except the
+# number 0 itself.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in each linked list is in the range [1, 100].
+# 0 <= Node.val <= 9
+# It is guaranteed that the list represents a number that does not have leading
+# zeros.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
+
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
+# @lc code=end
+
