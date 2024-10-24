6b1f95e4a5532ada65d71f869c4f21fea5aa7bae
bon <badfatcat.git@gmail.com>
2024-10-24 20:52:10 +0900

    leet code mission of the day

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -15,61 +15,63 @@
 #
 # Given a string s containing just the characters '(', ')', '{', '}', '[' and
 # ']', determine if the input string is valid.
-# 
+#
 # An input string is valid if:
-# 
-# 
+#
+#
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
 # Every close bracket has a corresponding open bracket of the same type.
-# 
-# 
-# 
+#
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: s = "()"
-# 
+#
 # Output: true
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: s = "()[]{}"
-# 
+#
 # Output: true
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: s = "(]"
-# 
+#
 # Output: false
-# 
-# 
+#
+#
 # Example 4:
-# 
-# 
+#
+#
 # Input: s = "([])"
-# 
+#
 # Output: true
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= s.length <= 10^4
 # s consists of parentheses only '()[]{}'.
-# 
-# 
+#
+#
 #
 
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        while '()' in s or '[]'in s or '{}' in s:
+            s = s.replace('()','').replace('[]','').replace('{}','')
+        return False if len(s) !=0 else True
 # @lc code=end
 
